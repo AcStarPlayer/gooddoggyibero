b19757945349c89c7bc172633b722266d91b8859
Brayan Andres Castro Marquez <bkpunk1992@gmail.com>
2025-12-03 22:35:08 -0500

    se actualizan .env y otros scripts

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,9 +3,7 @@
 // =====================================================
 
 const bcrypt = require("bcryptjs");
-
 const jwt = require("jsonwebtoken");
-
 const User = require("../models/user.model");
 
 // =====================================================
